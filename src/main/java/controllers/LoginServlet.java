c1960d3e872fdf6d7bf327b2f5855f25dac39685
Alex Perez <alex.f.perez92@gmail.com>
2023-06-05 22:04:29 -0500

    sessions and cookies exercise

--- src/main/java/controllers/LoginServlet.java
+++ src/main/java/controllers/LoginServlet.java
@@ -5,25 +5,25 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
-@WebServlet(name = "controllers.LoginServlet", urlPatterns = "/login")
+@WebServlet(name = "LoginServlet", urlPatterns = "/login")
 public class LoginServlet extends HttpServlet {
-    @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        req.getRequestDispatcher("/users/login.jsp").forward(req, resp);
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        request.getRequestDispatcher("/login.jsp").forward(request, response);
     }
 
-    @Override
-    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        String username = req.getParameter("username");
-        String password = req.getParameter("password");
-
-        if (username.equals("admin") && password.equals("password")) {
-            resp.sendRedirect("/profile");
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        String username = request.getParameter("username");
+        String password = request.getParameter("password");
+        boolean validAttempt = username.equals("admin") && password.equals("password");
+        HttpSession session = request.getSession();
+        if (validAttempt) {
+            session.setAttribute("user", username);
+            response.sendRedirect("/profile");
         } else {
-            resp.sendRedirect("/login");
+            response.sendRedirect("/login");
         }
-
     }
 }
